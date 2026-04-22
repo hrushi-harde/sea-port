ba7533f0995aafac7d5ca8fcac8622d988a9c67c
Hrushikesh Harde <178544325+hrushi-harde@users.noreply.github.com>
2026-04-22 15:55:59 +0530

    Update ports.routes.js

--- routes/ports.routes.js
+++ routes/ports.routes.js
@@ -1,28 +1,21 @@
 import { Router } from "express";
 const router = Router();
-import { deletePort, getAllPorts } from "../controllers/ports.controller.js";
-import { getPortByCode } from "../controllers/ports.controller.js";
-import { createPort } from "../controllers/ports.controller.js";
-import { updatePort } from "../controllers/ports.controller.js";
+import { getAllPorts, getPortByCode, createPort, deletePort} from "../controllers/ports.controller.js";
 
 router.get('/',(req,res)=>{
-    getAllPorts(req, res);
+  getAllPorts(req, res);
 })
 
 router.get('/:code',(req,res)=>{
-      getPortByCode(req, res);
-    });
+   getPortByCode(req, res);
+ });
 
 router.post("/",(req,res)=>{
-    createPort(req, res);
-})
+  createPort(req,res);
+});
 
 router.delete("/:code",(req,res)=>{
-    deletePort(req, res);
+   deletePort(req,res);
 })
-
-router.patch("/:code", (req, res) => {
-    updatePort(req, res);
-});
 
 export default router;
